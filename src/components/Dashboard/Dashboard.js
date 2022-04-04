54bbed93ca01c7826d5c88873227f94cfca9d324
rakibul-cse-diu <rakibul2180@gmail.com>
2022-04-04 13:49:55 +0600

    add charts on dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import BarCharts from '../Charts/BarCharts';
+import LineCharts from '../Charts/LineChart';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>This is dashboard component</h1>
+        <div className='d-flex flex-column flex-md-row flex-lg-row'>
+            <div className='m-5'>
+                <h1>MONTH WISE SELL</h1>
+                <LineCharts></LineCharts>
+            </div>
+            <div className='m-5'>
+                <h1>INVESTMENT vs REVENUE</h1>
+                <BarCharts />
+            </div>
         </div>
     );
 };
